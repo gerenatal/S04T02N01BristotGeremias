f01cf950d8f50b5c70364d1afc188973d9488c1d
gerenatal <gbristot@hotmail.com>
2024-05-14 01:43:26 +0200

    Initial commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/bristot/geremias/s04/t02/n01/model/domain/Fruit.java
@@ -0,0 +1,48 @@
+package cat.itacademy.barcelonactiva.bristot.geremias.s04.t02.n01.model.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Fruit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private int quantityKilos;
+
+    public Fruit(){}
+
+    public Fruit(int id, String name, int quantityKilos) {
+        this.id = id;
+        this.name = name;
+        this.quantityKilos = quantityKilos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantityKilos() {
+        return quantityKilos;
+    }
+
+    public void setQuantityKilos(int quantityKilos) {
+        this.quantityKilos = quantityKilos;
+    }
+}
